e3b6674ea42d12c0ce9ed9f13caaa4937fec42b6
EGBAR <egbar.better@gmail.com>
2019-10-21 22:46:26 +0800

    Complete wechat+dva

---
+++ src/app.config.js
@@ -0,0 +1,9 @@
+const pages = ['pages/index/index', 'pages/new/index'];
+
+module.exports = {
+  pages,
+  window: {
+    navigationBarTitleText: 'Remax Wechat Template',
+    navigationBarBackgroundColor: '#282c34'
+  }
+};
